33b7df69592ec66e4a3cf297ad2fa9a9c22a87ad
sarvesh pednekar <sarveshpednekar30@gmail.com>
2024-11-23 00:20:03 +0530

    setup : datasource

---
+++ src/test/java/org/dev/coupons/CouponManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev.coupons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
